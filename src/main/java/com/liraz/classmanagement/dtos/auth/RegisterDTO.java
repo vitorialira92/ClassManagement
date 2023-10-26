93224653450cf819f39361a516672679f23b513e
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-10-26 12:57:17 -0300

    fix: delete RegisterDTO

--- src/main/java/com/liraz/classmanagement/dtos/auth/RegisterDTO.java
+++
@@ -1,7 +0,0 @@
-package com.liraz.classmanagement.dtos.auth;
-
-import com.liraz.classmanagement.domain.user.UserRole;
-import jakarta.validation.constraints.NotNull;
-
-public record RegisterDTO(@NotNull String login, @NotNull String password, @NotNull UserRole userRole) {
-}
