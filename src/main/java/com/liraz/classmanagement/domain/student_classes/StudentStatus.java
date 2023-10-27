5c1cf07fb05233488d637319ea07e001cac62990
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-10-27 16:11:01 -0300

    feat: detailing for swagger

--- src/main/java/com/liraz/classmanagement/domain/student_classes/StudentStatus.java
+++ src/main/java/com/liraz/classmanagement/domain/student_classes/StudentStatus.java
@@ -1,9 +1,15 @@
 package com.liraz.classmanagement.domain.student_classes;
 
+import com.fasterxml.jackson.databind.annotation.EnumNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
 public enum StudentStatus {
+    @Schema(description = "Student is approved.")
     PASSED("passed"),
+    @Schema(description = "Student dropped the class.")
     DROPPED("dropped"),
+    @Schema(description = "Student failed the class.")
     FAILED("failed"),
+    @Schema(description = "Student is currently studying.")
     ONGOING("ongoing");
 
     private String studentStatus;
