e9d9bc2954e3e38a349066e6f0c97a29d0d6a7d9
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-10-27 16:10:15 -0300

    feat: detailing for swagger

--- src/main/java/com/liraz/classmanagement/dtos/student_class/StudentEnrollmentDetailDTO.java
+++ src/main/java/com/liraz/classmanagement/dtos/student_class/StudentEnrollmentDetailDTO.java
@@ -1,9 +1,13 @@
 package com.liraz.classmanagement.dtos.student_class;
 
 import com.liraz.classmanagement.domain.student_classes.StudentStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "Contains student registration number and student status in a class.")
 public class StudentEnrollmentDetailDTO {
+    @Schema(description = "Student registration number", example = "1058")
     private int studentRegistration;
+    @Schema(description = "Student's status", example = "FAILED")
     private StudentStatus studentStatus;
 
     public int getStudentRegistration() {
