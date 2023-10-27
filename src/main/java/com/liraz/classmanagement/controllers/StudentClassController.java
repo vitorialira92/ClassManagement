9133a7f4764770c72c10d3584a437cdf4295ee97
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-10-27 14:27:13 -0300

    feat: detailing for swagger

--- src/main/java/com/liraz/classmanagement/controllers/StudentClassController.java
+++ src/main/java/com/liraz/classmanagement/controllers/StudentClassController.java
@@ -6,6 +6,10 @@ import com.liraz.classmanagement.dtos.student_class.ClassRecordDTO;
 import com.liraz.classmanagement.dtos.student_class.EnrollInClassDTO;
 import com.liraz.classmanagement.services.StudentClassService;
 import com.liraz.classmanagement.services.email.EmailSenderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Management of class enrollment")
 @RestController
 @RequestMapping("/student_class")
 public class StudentClassController {
@@ -23,6 +28,9 @@ public class StudentClassController {
     @Autowired
     private EmailSenderService emailSenderService;
 
+    @Operation(summary = "Enroll a student in a class.",
+                description = "Enroll a student in a class, if the semester is on " +
+                        "REGISTRATION period and the student is active.")
     @PostMapping() //OK
     public ResponseEntity<?> enrollInClass(@RequestBody EnrollInClassDTO enrollInClassDTO) throws MessagingException {
 
@@ -33,6 +41,9 @@ public class StudentClassController {
         return new ResponseEntity<StudentClass>(studentClass, HttpStatus.CREATED);
 
     }
+    @Operation(summary = "Remove enrollment in a class or drop the class, depending on the period of the semester.",
+            description = "Remove enrollment in a class if the semester is on REGISTRATION period." +
+                    "Drop the class if the semester is ONGOING.")
     @DeleteMapping //ok
     public ResponseEntity<?> deleteEnrollment(@RequestBody EnrollInClassDTO enrollInClassDTO) throws MessagingException {
        service.removeEnrollment(enrollInClassDTO.getClassCode(),
@@ -41,36 +52,46 @@ public class StudentClassController {
        return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/current/{registration}") //ok
+    @Operation(summary = "Get all classes a student is enrolled in for the ongoing semesters.")
+    @GetMapping("/current/{registration}")
     public ResponseEntity<?> fetchClassesForStudentInCurrentSemester(
-            @PathVariable int registration){
+            @Parameter(description = "Registration number of the student whose classes you want to fetch.")
+                @PathVariable int registration){
         return new ResponseEntity<List<StudentClass>>(
                 service.fetchClassesForStudentInCurrentSemester(registration),
                     HttpStatus.OK);
     }
 
+    @Operation(summary = "Get all classes a student is enrolled in for the semesters on registration period.")
     @GetMapping("/in_registration/{registration}") //ok
     public ResponseEntity<?> fetchClassesForStudentInRegistrationPeriod(
+            @Parameter(description = "Registration number of the student whose classes you want to fetch.")
             @PathVariable int registration){
         return new ResponseEntity<List<StudentClass>>(
                 service.fetchClassesForStudentInRegistrationPeriod(registration),
                 HttpStatus.OK);
     }
 
+    @Operation(summary = "Get all students currently enrolled in a class.")
     @GetMapping("/class/{classCode}") //ok
-    public ResponseEntity<?> getAllStudentsInAClass(@PathVariable String classCode){
+    public ResponseEntity<?> getAllStudentsInAClass(
+            @Parameter(description = "Code of the class whose students you want to fetch.")
+            @PathVariable String classCode){
         return new ResponseEntity<List<Student>>(service.getAllStudentsInAClass(classCode),
                 HttpStatus.OK);
     }
 
+    @Operation(summary = "Set the status of all students (PASSED, FAILED) in a class.")
     @PostMapping("/results")
     public ResponseEntity<?> setClassResults(@RequestBody ClassRecordDTO classRecordDTO){
         service.setClassResults(classRecordDTO);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Get the status of all students (PASSED, FAILED, DROPPED) in a class.")
     @GetMapping("/results/{classCode}")
-    public ResponseEntity<?> getClassResults(@PathVariable String classCode){
+    public ResponseEntity<?> getClassResults(@Parameter(description = "Code of the class whose students' " +
+            "status you want to fetch.") @PathVariable String classCode){
         return new ResponseEntity<ClassRecordDTO>(service.getClassResults(classCode), HttpStatus.OK);
     }
 }
